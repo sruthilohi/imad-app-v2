51744b3548663d5ecea9d105660d9e197c8414de
sruthi lohithakshan <sruthilohi@gmail.com>
2017-03-19 15:56:57 +0400

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -6,7 +6,7 @@ function loadCommentForm () {
         <h5>Submit a comment</h5>
         <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
         <br/>
-      //  <input type="submit" id="submit" value="Submit" />
+     
        <button type="button" id="article_btn" class="btn btn-info">
                 <span class="glyphicon "></span>Submit
               </button>
